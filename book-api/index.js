84414348b5c5f08840911d3f48cc03bd313356bb
wptasmina <wptasmina@gmail.com>
2025-07-02 22:46:39 +0600

    add server

--- book-api/index.js
+++ book-api/index.js
@@ -1,1 +1,8 @@
-const express =requard("express")
+const express = require("express")
+const app = express()
+const PORT = 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+    
+})
